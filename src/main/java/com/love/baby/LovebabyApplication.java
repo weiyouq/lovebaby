225e74e87d55150c7cfb4106e58da90d487cf428
weiyouq <jiayouk@163.com>
2020-05-10 20:44:29 +0800

    firest commit

---
+++ src/main/java/com/love/baby/LovebabyApplication.java
@@ -0,0 +1,13 @@
+package com.love.baby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovebabyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovebabyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/love/baby/LovebabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.baby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovebabyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
